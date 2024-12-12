24429d089df568ddb0cd3786b1c14531f7462687
Ehte22 <syedehteshamali22@gmail.com>
2024-12-12 21:39:52 +0530

    auth update again

--- auth/client/webpack.config.js
+++ auth/client/webpack.config.js
@@ -10,7 +10,7 @@ const printCompilationMessage = require('./compilation.config.js');
 module.exports = (_, argv) => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'remoteEntry.js',
     // publicPath: 'http://localhost:3001/'
     publicPath: 'https://react-micro-frontend-auth.vercel.app/'
   },

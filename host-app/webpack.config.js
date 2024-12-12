502d2581d3146359782cb5b6ec25fc4f4770016d
Ehte22 <syedehteshamali22@gmail.com>
2024-12-12 21:29:58 +0530

    host update

--- host-app/webpack.config.js
+++ host-app/webpack.config.js
@@ -9,10 +9,10 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: 'main.js',
-    // publicPath: 'https://react-micro-frontend-zeta.vercel.app/'
-    publicPath: 'http://localhost:3000/'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: 'https://react-micro-frontend-zeta.vercel.app/'
+    // publicPath: 'http://localhost:3000/'
   },
 
   resolve: {
@@ -68,7 +68,7 @@ module.exports = (_, argv) => ({
       name: "host_app",
       filename: "remoteEntry.js",
       remotes: {
-        auth: "auth@http://localhost:3001/remoteEntry.js",
+        auth: "auth@https://react-micro-frontend-auth.vercel.app/remoteEntry.js",
         product: "product@http://localhost:3002/remoteEntry.js",
         layout: "layout@http://localhost:3003/remoteEntry.js",
       },
